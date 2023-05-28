d3beb9ba668797ee52dc6c03a9391372e7cfaf76
Mario Rotz <mariorotz@mobiletrend.de>
2023-05-28 22:18:35 +0200

    Delivers connectivity error on client error

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -9,17 +9,24 @@ import Foundation
 public final class RemoteFeedLoader {
     private let url : URL
     private let client : HTTPClient
-
+    
+    public enum Error : Swift.Error {
+        case  connectivity
+    }
     public init(url: URL = URL(string:"https://a-url.com")!, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    public func load() {
-        client.get(from: url)
+    public func load( completion: @escaping (Error)-> Void = { _ in }) {
+        client.get(from: url) {
+            error in
+            completion(.connectivity)
+            
+        }
     }
 }
 
 public protocol HTTPClient {
-    func get(from url:URL)
+    func get(from url:URL, completion: @escaping (Error) -> Void)
 }
