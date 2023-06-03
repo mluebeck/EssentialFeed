f25af403070eeb2ddf6a53299611833f6702a7b3
Mario Rotz <mariorotz@mobiletrend.de>
2023-06-04 00:00:35 +0200

    Extract setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,20 +11,7 @@ import EssentialFeed
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string:"https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url:testServerURL,client:client)
-        
-        let exp = expectation(description: "wait for load completion")
-        var receivedResult : LoadFeedResult?
-        
-        loader.load  {
-            result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        
+        let receivedResult = self.getFeedResult()
         switch receivedResult {
         case let .success(items)?:
             XCTAssertEqual(items.count,8, "expected 8 items in the test account feed")
@@ -40,6 +27,22 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
     //MARK: - helper methods
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string:"https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url:testServerURL,client:client)
+        
+        let exp = expectation(description: "wait for load completion")
+        var receivedResult : LoadFeedResult?
+        
+        loader.load  {
+            result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+       return receivedResult
+    }
     
     private func expectedItems(at  index: Int ) -> FeedItem {
         return FeedItem(id: id(at:index), description: description(at:index), location: location(at:index), imageURL: imageURL(at:index))
