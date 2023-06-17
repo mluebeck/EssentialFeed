aa8bed2d7a0b1938bebf05038f33991ce2bbfef8
Mario Rotz <mariorotz@mobiletrend.de>
2023-06-17 16:19:06 +0200

    Does not delete cache upon "LocalFeedLoader" creation

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,24 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Mario Rotz on 17.06.23.
+//
+
+import XCTest
+class LocalFeedLoader {
+    init(store:FeedStore) {
+        
+    }
+}
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+final class CacheFeedUseCaseTests: XCTestCase {
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store:store)
+        XCTAssertEqual(store.deleteCachedFeedCallCount,0)
+    }
+}
