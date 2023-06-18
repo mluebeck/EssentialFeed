2f38c3248c21968406f6250db26eba50d88d3679
Mario Rotz <mariorotz@mobiletrend.de>
2023-06-18 14:10:50 +0200

    Add "LocalFeedItem" data transfer representation to decouple storage frameworks from "FeedItem" data models

--- EssentialFeed/Feed API/HTTPClient.swift
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -11,9 +11,6 @@ public enum HTTPClientResult {
     case failure(Error)
 }
 
-
 public protocol HTTPClient {
     func get(from url:URL, completion: @escaping (HTTPClientResult) -> Void)
 }
-
-
--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -36,10 +36,11 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let timestamp = Date()
         
         let items = [uniqueItem(),uniqueItem()]
+        let localItems = items.map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.imageURL)}
         let (sut,store) = makeSUT(currentDate: { timestamp } )
         sut.save(items) { _ in }
         store.completeDeletionSuccessfully()
-        XCTAssertEqual(store.receivedMessages,[.deleteCachedFeed, .insert(items,timestamp)])
+        XCTAssertEqual(store.receivedMessages,[.deleteCachedFeed, .insert(localItems,timestamp)])
     }
     
     func test_save_failsOnDeletionError() {
@@ -130,7 +131,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
         enum ReceivedMessage : Equatable {
             case deleteCachedFeed
-            case insert([FeedItem],Date)
+            case insert([LocalFeedItem],Date)
         }
         
         private(set) var receivedMessages = [ReceivedMessage]()
@@ -152,7 +153,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
             deletionCompletions[index](nil)
         }
         
-        func insert(_ items: [FeedItem],timestamp:Date,completion: @escaping InsertionCompletion) {
+        func insert(_ items: [LocalFeedItem],timestamp:Date,completion: @escaping InsertionCompletion) {
             insertionCompletions.append(completion)
             receivedMessages.append(.insert(items,timestamp))
         }
