e8629169dc318ff3f8f6aa04b7a077b9eddae456
Mario Rotz <mariorotz@mobiletrend.de>
2023-06-22 13:52:00 +0200

    Separate "Date" extension helpers into distinct contexts to clarify their scope (one is a cache-policy specific DSL and the other is just a reusable DSL helper)

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -42,7 +42,7 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         
-        let nonExpiredTimestamp = fixedCurrentDate.adding(days:-7).adding(seconds:1)
+        let nonExpiredTimestamp = fixedCurrentDate.minusFeedCacheMaxAge().adding(seconds:1)
         let (sut,store) = makeSUT(currentDate: {fixedCurrentDate})
         expect(sut,toCompleteWith: .success(feed.models),when: {
             store.completeRetrieval(with:feed.local, timestamp:nonExpiredTimestamp)
