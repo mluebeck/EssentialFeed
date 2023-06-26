0e25df1aef00e0cc152097eefd77e3d6df266501
Mario Rotz <mariorotz@mobiletrend.de>
2023-06-26 21:15:42 +0200

    Add 'EssentialFeedCacheIntegrationTests' target to separate the potentially-slower cash integration tests from the fast unit/isolated tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Mario Rotz on 26.06.23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+     
+
+}
