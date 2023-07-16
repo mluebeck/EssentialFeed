b55f8ad12fe3ca0f9e572ecaaa6029a1de8972bc
Mario Rotz <mariorotz@mobiletrend.de>
2023-07-17 01:49:20 +0200

    Cancel image URL preloading when image view is not near visible anymore

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -22,8 +22,19 @@ final public class FeedViewController : UITableViewController, UITableViewDataSo
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
             let cellModel = tableModel[indexPath.row]
-            _ = imageLoader?.loadImageData(from: cellModel.url) { _ in }
+            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
         }
+    }
+    
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach { indexPath in
+           cancelTask(forRowAt: indexPath)
+        }
+    }
+    
+    private func cancelTask(forRowAt indexPath: IndexPath) {
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
     }
     
     private var feedLoader: FeedLoader?
@@ -87,7 +98,8 @@ final public class FeedViewController : UITableViewController, UITableViewDataSo
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+        cancelTask(forRowAt: indexPath)
     }
+    
+    
 }
