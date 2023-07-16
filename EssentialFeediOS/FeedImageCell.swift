0962fac6eb14dc7613b5e225f6d29ae58bea24e2
Mario Rotz <mariorotz@mobiletrend.de>
2023-07-16 23:25:09 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mario Rotz on 16.07.23.
+//
+
+import UIKit
+
+public final class FeedImageCell : UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    
+}
