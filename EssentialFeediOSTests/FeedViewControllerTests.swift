885c9ea355b5a1686cd42fb1a856ed2ae706d2b9
Mario Rotz <mariorotz@mobiletrend.de>
2023-07-17 02:04:13 +0200

    fixing iOS 16.1 issues with UIImage+ Data

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -136,13 +136,13 @@ final class FeedViewControllerTests: XCTestCase {
 
             let imageData0 = UIImage.make(withColor: .red).pngData()!
             loader.completeImageLoading(with: imageData0, at: 0)
-            XCTAssertEqual(UIImage.init(data: (view0?.renderedImage)!)?.pngData(), UIImage.init(data: (imageData0))?.pngData(), "Expected image for first view once first image loading completes successfully")
+            XCTAssertEqual(view0?.renderedImage, imageData0, "Expected image for first view once first image loading completes successfully")
             XCTAssertEqual(view1?.renderedImage, .none, "Expected no image state change for second view once first image loading completes successfully")
 
             let imageData1 = UIImage.make(withColor: .blue).pngData()!
             loader.completeImageLoading(with: imageData1, at: 1)
-            XCTAssertEqual(UIImage.init(data: (view0?.renderedImage)!)?.pngData(), UIImage.init(data: (imageData0))?.pngData(), "Expected no image state change for first view once second image loading completes successfully")
-            XCTAssertEqual(UIImage.init(data: (view1?.renderedImage)!)?.pngData(), UIImage.init(data: (imageData1))?.pngData(), "Expected image for second view once second image loading completes successfully")
+            XCTAssertEqual(view0?.renderedImage, imageData0, "Expected no image state change for first view once second image loading completes successfully")
+            XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
     }
 
     func test_feedImageViewRetryButton_isVisibleOnImageURLLoadError() {
@@ -449,12 +449,11 @@ private extension UIRefreshControl {
 private extension UIImage {
     static func make(withColor color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContext(rect.size)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
     }
 }
