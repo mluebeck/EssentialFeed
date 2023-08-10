ce5fd6aa6dc54ba1961369647e7069aa5bb32b86
mluebeck <mariorotz@googlemail.com>
2023-08-10 15:11:20 +0200

    Move 'FeedCache' to the 'Feed Feature' production group

---
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedCache.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCache.swift
+//  EssentialFeed
+//
+//  Created by Mario Rotz on 10.08.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+public protocol FeedCache {
+    typealias Result = Swift.Result<Void,Error>
+    func save(_ feed: [FeedImage], completion: @escaping (Result)->Void)
+}
