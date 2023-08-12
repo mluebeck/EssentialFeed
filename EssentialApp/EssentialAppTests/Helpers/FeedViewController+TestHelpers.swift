5d06115736ed18b64a92d1753942debf54e6f732
mluebeck <mariorotz@googlemail.com>
2023-08-12 22:18:05 +0200

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -6,40 +6,44 @@ import UIKit
 import EssentialFeediOS
 
 extension FeedViewController {
-	func simulateUserInitiatedFeedReload() {
-		refreshControl?.simulatePullToRefresh()
-	}
-	
-	@discardableResult
-	func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-		return feedImageView(at: index) as? FeedImageCell
-	}
-	
-	@discardableResult
-	func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
-		let view = simulateFeedImageViewVisible(at: row)
-		
-		let delegate = tableView.delegate
-		let index = IndexPath(row: row, section: feedImagesSection)
-		delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
-		
-		return view
-	}
-	
-	func simulateFeedImageViewNearVisible(at row: Int) {
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView(tableView, prefetchRowsAt: [index])
-	}
-	
-	func simulateFeedImageViewNotNearVisible(at row: Int) {
-		simulateFeedImageViewNearVisible(at: row)
-		
-		let ds = tableView.prefetchDataSource
-		let index = IndexPath(row: row, section: feedImagesSection)
-		ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
-	}
-	
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
+        return feedImageView(at: index) as? FeedImageCell
+    }
+    
+    @discardableResult
+    func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
+        let view = simulateFeedImageViewVisible(at: row)
+        
+        let delegate = tableView.delegate
+        let index = IndexPath(row: row, section: feedImagesSection)
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+        
+        return view
+    }
+    
+    func simulateFeedImageViewNearVisible(at row: Int) {
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView(tableView, prefetchRowsAt: [index])
+    }
+    
+    func simulateFeedImageViewNotNearVisible(at row: Int) {
+        simulateFeedImageViewNearVisible(at: row)
+        
+        let ds = tableView.prefetchDataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
+    }
+    
+    func renderedFeedImageData(at index: Int) -> Data? {
+        return simulateFeedImageViewVisible(at: index)?.renderedImage
+    }
+    
 	var errorMessage: String? {
 		return errorView?.message
 	}
