b2da27a638f02a1128ae2863c1b41e7dc079c130
mluebeck <mariorotz@googlemail.com>
2023-08-15 22:09:59 +0200

    Replace FeedLoader composition with Combine operators

--- EssentialApp/EssentialApp/MainQueueDispatchDecorator.swift
+++ EssentialApp/EssentialApp/MainQueueDispatchDecorator.swift
@@ -5,6 +5,7 @@
 import Foundation
 import EssentialFeed
 
+
 final class MainQueueDispatchDecorator<T> {
 	private let decoratee: T
 	
@@ -20,14 +21,7 @@ final class MainQueueDispatchDecorator<T> {
 		completion()
 	}
 }
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-	func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		decoratee.load { [weak self] result in
-			self?.dispatch { completion(result) }
-		}
-	}
-}
+ 
 
 extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
 	func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
