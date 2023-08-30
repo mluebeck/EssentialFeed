cfe123aed81a3b6596dd9d469a9800d7c394c07d
mluebeck <mariorotz@googlemail.com>
2023-08-30 11:27:25 +0200

    Adding test and prod file for ImageCommentsMapper

---
+++ EssentialFeed/EssentialFeed/ImageComments/API/ImageCommentsMapper.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCommentsMapper.swift
+//  EssentialFeed
+//
+//  Created by Mario Rotz on 30.08.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+import Foundation
+
+public final class ImageCommentsMapper {}
+
+ 
---
+++ EssentialFeed/EssentialFeedTests/ImageComments/API/ImageCommentsMapperTests.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCommentsMapperTests.swift
+//  EssentialFeedTests
+//
+//  Created by Mario Rotz on 30.08.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsMapperTests: XCTestCase {}
