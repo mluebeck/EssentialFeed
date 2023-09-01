98e0d7c80cc8412c4fa2a1b3218b61dda3d255a2
mluebeck <mariorotz@googlemail.com>
2023-09-02 01:54:28 +0200

    Replace RemoteFeedImageDataLoader with HTTPClient publisher composed with FeedImageDataMapper

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -68,11 +68,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     
     private func makeLocalImageLoaderWithRemoteFallback(url:URL)->FeedImageDataLoader.Publisher {
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+ 
         let localImageLoader = LocalFeedImageDataLoader(store: store)
-        return localImageLoader.loadImageDataPublisher(from: url).fallback(to: {
-            remoteImageLoader.loadImageDataPublisher(from: url).caching(to: localImageLoader, using: url)
-        })
+        return localImageLoader
+            .loadImageDataPublisher(from: url)
+            .fallback(to: { [httpClient] in
+                httpClient
+                    .getPublisher(url: url)
+                    .tryMap(FeedImageDataMapper.map)
+                    .caching(to: localImageLoader, using: url)
+            })
+        
     }
 }
 
