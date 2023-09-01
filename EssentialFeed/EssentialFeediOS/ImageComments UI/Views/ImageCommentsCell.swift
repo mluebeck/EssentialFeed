62b60e3050a623b7436f702699a1c4ff4f190dc9
mluebeck <mariorotz@googlemail.com>
2023-09-01 19:48:43 +0200

    adding ImageCommentCellController, ImageCommentCell and building the storyboard file with ListViewController + ImageCommentCellController and ImageCommentCell

---
+++ EssentialFeed/EssentialFeediOS/ImageComments UI/Views/ImageCommentsCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentsCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mario Rotz on 01.09.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
