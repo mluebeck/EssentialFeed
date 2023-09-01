fe8fd2bea3f8f0d9d45f63cc786b8b81aadcf079
mluebeck <mariorotz@googlemail.com>
2023-09-01 19:41:38 +0200

    adding UI components: ImageCommentsCellController, ImageCommentsSnapshotTests, ImageComments Storyboard

---
+++ EssentialFeed/EssentialFeediOS/ImageComments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Mario Rotz on 01.09.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Mario Rotz on 01.09.23.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+    func test_feedWithContent() {}
+
+    func test_feedWithFailedImageLoading() {}
+}
