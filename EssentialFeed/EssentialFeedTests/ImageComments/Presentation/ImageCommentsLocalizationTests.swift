5c126eaea70055fc614bd9f3a8e254f725d2af44
mluebeck <mariorotz@googlemail.com>
2023-09-01 19:33:17 +0200

    implementing localization tests and adding english,greek,portugese language strings

--- EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentsLocalizationTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentsLocalizationTests.swift
@@ -10,6 +10,9 @@ import XCTest
 import EssentialFeed
 
 final class ImageCommentsLocalizationTests: XCTestCase {
-    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {}
-    
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
 }
