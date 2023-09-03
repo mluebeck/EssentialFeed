a53c85397264c417e1b53eb9ff2bbd22c10585a7
mluebeck <mariorotz@googlemail.com>
2023-09-04 00:22:55 +0200

    Replace #file with the new #filePath

--- EssentialFeed/EssentialFeedTests/Helpers/SharedLocalizationTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedLocalizationTestHelpers.swift
@@ -9,7 +9,7 @@
 import Foundation
 import XCTest
 
-func assertLocalizedKeyAndValuesExist(in presentationBundle: Bundle, _ table: String, file: StaticString = #file, line: UInt = #line) {
+func assertLocalizedKeyAndValuesExist(in presentationBundle: Bundle, _ table: String, file: StaticString = #filePath, line: UInt = #line) {
     let localizationBundles = allLocalizationBundles(in: presentationBundle, file: file, line: line)
     let localizedStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table, file: file, line: line)
     
@@ -28,7 +28,7 @@ func assertLocalizedKeyAndValuesExist(in presentationBundle: Bundle, _ table: St
 
 private typealias LocalizedBundle = (bundle: Bundle, localization: String)
 
-private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #file, line: UInt = #line) -> [LocalizedBundle] {
+private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #filePath, line: UInt = #line) -> [LocalizedBundle] {
     return bundle.localizations.compactMap { localization in
         guard
             let path = bundle.path(forResource: localization, ofType: "lproj"),
@@ -42,7 +42,7 @@ private func allLocalizationBundles(in bundle: Bundle, file: StaticString = #fil
     }
 }
 
-private func allLocalizedStringKeys(in bundles: [LocalizedBundle], table: String, file: StaticString = #file, line: UInt = #line) -> Set<String> {
+private func allLocalizedStringKeys(in bundles: [LocalizedBundle], table: String, file: StaticString = #filePath, line: UInt = #line) -> Set<String> {
     return bundles.reduce([]) { (acc, current) in
         guard
             let path = current.bundle.path(forResource: table, ofType: "strings"),
