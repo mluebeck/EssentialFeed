82d4a249c9fe61249efbb9ada4d3f275a38815dd
mluebeck <mariorotz@googlemail.com>
2023-09-06 19:49:32 +0200

    Make CoreData FeedImageDataStore implementation sync

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -85,19 +85,14 @@ class CoreDataFeedImageDataStoreTests: XCTestCase {
 	}
 
 	private func expect(_ sut: CoreDataFeedStore, toCompleteRetrievalWith expectedResult: FeedImageDataStore.RetrievalResult, for url: URL,  file: StaticString = #filePath, line: UInt = #line) {
-		let exp = expectation(description: "Wait for load completion")
-		sut.retrieve(dataForURL: url) { receivedResult in
-			switch (receivedResult, expectedResult) {
-			case let (.success( receivedData), .success(expectedData)):
-				XCTAssertEqual(receivedData, expectedData, file: file, line: line)
-				
-			default:
-				XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-			}
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1.0)
-	}
+        let receivedResult = Result { try sut.retrieve(dataForURL: url) }
+        switch (receivedResult, expectedResult) {
+        case let (.success( receivedData), .success(expectedData)):
+            XCTAssertEqual(receivedData, expectedData, file: file, line: line)
+        default:
+            XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+        }
+    }
 
 	private func insert(_ data: Data, for url: URL, into sut: CoreDataFeedStore, file: StaticString = #filePath, line: UInt = #line) {
 		let exp = expectation(description: "Wait for cache insertion")
